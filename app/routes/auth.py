981a6266920e8bfd8aa919e40f3ef921ad4a8377
Despot Maksimovic <despotmaksimovic@gmail.com>
2025-11-16 01:17:13 +0200

    fixed auth.py

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,3 @@
-from idlelib.query import Query
-
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from app import db, bcrypt
